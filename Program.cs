724c01e110b9c9107786f7c63d476641e28b6d29
Brady Gaster <bradyg@microsoft.com>
2019-05-09 22:45:10 -0700

    initial creation

---
+++ Program.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration.AzureAppConfiguration;
+using Microsoft.FeatureManagement;
+
+namespace FeatureFlags
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    var settings = config.Build();
+                    config.AddAzureAppConfiguration(options => 
+                    {
+                        var connectionString = settings["ConnectionStrings:AppConfig"];
+                        options
+                            .Connect(connectionString)
+                            .Watch(Features.EnableInformationLogs, TimeSpan.FromMinutes(1))
+                            .UseFeatureFlags();
+                    });
+                })
+                .ConfigureServices(services =>
+                {
+                    services.AddFeatureManagement();
+                    services.AddHostedService<Worker>();
+                });
+    }
+
+    public class Features
+    {
+        public static string EnableInformationLogs => "EnableInformationLogs";
+    }
+}
---
+++ Worker.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.FeatureManagement;
+
+namespace FeatureFlags
+{
+    public class Worker : BackgroundService
+    {
+        private readonly ILogger<Worker> _logger;
+        private readonly IServiceProvider _serviceProvider;
+
+        public Worker(ILogger<Worker> logger, 
+            IServiceProvider serviceProvider)
+        {
+            _logger = logger;
+            _serviceProvider = serviceProvider;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            using (IServiceScope scope = _serviceProvider.CreateScope())
+            {
+                var featureManager = 
+                    scope.ServiceProvider.GetRequiredService<IFeatureManager>();
+
+                while (!stoppingToken.IsCancellationRequested)
+                {
+                    if(featureManager.IsEnabled(Features.EnableInformationLogs))
+                        _logger.LogInformation("Worker running at: {time}", 
+                            DateTimeOffset.Now);
+                        
+                    await Task.Delay(1000, stoppingToken);
+                }
+            }
+        }
+    }
+}
